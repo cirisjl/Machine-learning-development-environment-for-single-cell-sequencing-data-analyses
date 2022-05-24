1a2ba1ec0ac305c9d540910c1f822f93ccb96b81
Jamal Saied <jas8dz@umsystem.edu>
2022-05-24 11:34:25 -0500

    adding auth

---
+++ fast_api/auth/credentials.py
@@ -0,0 +1,33 @@
+import yaml
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if os.getenv("place") == "local":
+    path = "/home/jsaied/api/"
+else:
+    path = "/home/jas8dz/Machine-learning-development-environment-for-single-cell-sequencing-data-analyses/fast_api/"
+
+
+with open(path + "config.yaml") as file:
+    yaml_file = yaml.load(file, Loader=yaml.FullLoader)
+    db = yaml_file["database"]
+    admin = yaml_file["admin"]
+    mode = yaml_file["mode"]
+
+
+db_credentials = db
+
+ADMIN_USER = admin["user"]
+ADMIN_PASSWORD = admin["hashed_password"]
+
+
+PRIV_KEY = open(os.path.join(path, "auth", "RS512.key"), "rb").read().decode("utf-8")
+PUB_KEY = open(os.path.join(path, "auth", "RS512.key.pub"), "rb").read().decode("utf-8")
+
+
+BEARER_TOKEN = os.getenv("token", "")
+SERVER_TOKEN = os.getenv("server_token", "")
+
+MODE = mode
