9b7eb35f3af30fde4e7000c764e088e140a4a9e9
Jamal Saied <jas8dz@umsystem.edu>
2022-05-25 10:35:37 -0500

    adding download feature

--- fast_api/routers/download/download.py
+++ fast_api/routers/download/download.py
@@ -10,6 +10,8 @@ import helper.jwt as jwt
 import helper.downloader_funcs as downloader
 from pydantic import AnyUrl
 from auth.credentials import PATH
+from fastapi.responses import FileResponse
+import os
 
 router = APIRouter(
     prefix="/download",
@@ -19,18 +21,18 @@ router = APIRouter(
 )
 
 
-@router.post(
-    "/save",
-)
+@router.post("/", response_class=FileResponse)
 async def post_download(
     url: AnyUrl,
 ):
 
     try:
         tmp = PATH + "tmp/"
-        # path = "/Users/jsaied/GitHub/Machine-learning-development-environment-for-single-cell-sequencing-data-analyses/fast_api/tmp"
         download_path = await downloader.async_download(url=url, folder=tmp, log=True)
-        # download_path = await downloader.download_url(url=url,folder=path, log=True)
-        return {"message": "Downloaded successfully", "path": download_path}
+
+        if os.path.exists(download_path):
+            filename = url.rpartition("/")[2]
+            return FileResponse(path=download_path, filename=filename)
+
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
