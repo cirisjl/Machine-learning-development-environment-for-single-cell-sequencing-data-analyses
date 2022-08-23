43d9bd4115db4332461f1456b8cdf1cab0a6262b
Cankun Wang <flykun0620@gmail.com>
2022-08-23 09:57:23 -0400

    add convert format

---
+++ convert_format/seurat_to_scanpy.R
@@ -0,0 +1,22 @@
+library(qs)
+library(Seurat)
+library(SeuratDisk)
+
+setwd('C:/Users/flyku/Documents/GitHub/Machine-learning-development-environment-for-single-cell-sequencing-data-analyses/benchmark')
+set.seed(42)
+
+# Read data
+combined <- qs::qread('./test.qsave')
+
+# Set default assay
+Idents(combined) <- combined$seurat_clusters
+DefaultAssay(combined) <- "RNA"
+
+# Export Seurat to AnnData
+SaveH5Seurat(combined, filename = paste0("test_raw.h5Seurat"),  overwrite = T)
+Convert(paste0("test_raw.h5Seurat"), dest = "h5ad", overwrite = T)
+
+# Import AnnData to Seurat
+Convert("test.h5ad", dest = "h5seurat", overwrite = TRUE)
+test_object <- LoadH5Seurat("test.h5seurat")
+test_object
