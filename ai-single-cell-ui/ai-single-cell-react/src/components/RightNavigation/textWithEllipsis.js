b046defc329876fb58a0e8361f0e921681f843f8
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-06-27 10:36:57 -0500

    Update textWithEllipsis.js

--- ai-single-cell-ui/ai-single-cell-react/src/components/RightNavigation/textWithEllipsis.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/RightNavigation/textWithEllipsis.js
@@ -1,20 +1,15 @@
 import React, { useState } from 'react';
 
 const TextWithEllipsis = ({ text, maxLength }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  const displayText = isExpanded ? text : text.slice(0, maxLength);
+  const displayText = text.length > maxLength ? text : text.slice(0, maxLength);
 
   return (
     <div className="text-container">
-      <div className="text-content" onMouseEnter={toggleExpand} onMouseLeave={toggleExpand}>
-        {displayText}
-      </div>
-      {!isExpanded && text.length > maxLength && <div className="ellipsis">...</div>}
+        <div className="text-content" title={text}>
+            {displayText}{text.length > maxLength && <div className="ellipsis">...</div>}
+
+        </div>
     </div>
   );
 };
