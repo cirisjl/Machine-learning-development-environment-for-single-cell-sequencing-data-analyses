2d93daac089d14634451bb37efb728ed32765344
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-08-03 10:47:37 -0500

    updates

--- ai-single-cell-ui/ai-single-cell-react/src/components/ToggleSwitch.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/ToggleSwitch.js
@@ -1,12 +1,12 @@
 import React from "react";
   
-const ToggleSwitch = ({ label }) => {
+const ToggleSwitch = ({ label , toggleSwitchForPublicDatasets}) => {
   return (
     <div>
       {label}{" "}
       <div className="toggle-switch">
         <input type="checkbox" className="checkbox" 
-               name={label} id={label} />
+               name={label} id={label} onClick={toggleSwitchForPublicDatasets} />
         <label className="label" htmlFor={label}>
           <span className="inner" />
           <span className="switch" />
