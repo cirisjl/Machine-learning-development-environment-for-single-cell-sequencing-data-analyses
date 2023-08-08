b2131231856db06096df218fbb08f27e06cbb59a
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-08-08 17:57:55 -0500

    Update run_integration.py

--- api/tools/run_integration.py
+++ api/tools/run_integration.py
@@ -49,7 +49,7 @@ def run_integration(task_id, datasets, inputs,userID,output, methods, species, d
         print(methods)
         print(input)
 
-        s = subprocess.call(["R -e \"rmarkdown::render('" + rmd_path + "', params=list(datasets='" + datasets + "', inputs='" + input + "', output_folder='" + output + "', output_format='" + output_format + "', methods='" + methods + "', default_assay='" + default_assay + "', genes='" + genes + "', reference='" + str(reference) + "', show_error='" + str(show_error) + "'), output_file='" + report_path + "')\""], shell = True)
+        s = subprocess.call(["R -e \"rmarkdown::render('" + rmd_path + "', params=list(datasets='" + str(datasets) + "', inputs='" + str(input) + "', output_folder='" + output + "', output_format='" + output_format + "', methods='" + str(methods) + "', default_assay='" + default_assay + "', genes='" + genes + "', reference='" + str(reference) + "', show_error='" + str(show_error) + "'), output_file='" + report_path + "')\""], shell = True)
         print(s)
     except Exception as e:
         print("Integration  is failed")
