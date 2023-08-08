bbf9fa0e4226d8798752b916c929bda523a8dadc
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-08-08 12:35:28 -0500

    Merged code from leijiang

--- api/celery_tasks/tasks.py
+++ api/celery_tasks/tasks.py
@@ -3,6 +3,8 @@ from celery import shared_task
 from tools.run_qc import run_qc
 from tools.run_normalization import run_normalization
 from tools.run_imputation import run_imputation
+from tools.run_integration import run_integration
+
 
 @shared_task(bind=True,autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 5},
              name='tools:create_qc_task')
@@ -25,4 +27,11 @@ def create_normalization_task(self, dataset, input, userID, output, methods,spec
 def create_imputation_task(self, dataset, input, userID, output, methods, layer=None, genes=None, ncores=12, show_error=True):
     task_id = self.request.id
     results = run_imputation(task_id,dataset, input, userID,  output, methods, layer=None, genes=None, ncores=12, show_error=True)
+    return results
+
+@shared_task(bind=True,autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 5},
+             name='tools:create_integration_task')
+def create_integration_task(self, datasets, inputs, userID, output, methods,species, default_assay='RNA', output_format='Seurat', genes=None, reference=12, show_error = True):
+    task_id = self.request.id
+    results = run_integration(task_id, datasets, inputs, userID, output, methods, species, default_assay='RNA', output_format='Seurat', genes=None, reference=12, show_error = True)
     return results
