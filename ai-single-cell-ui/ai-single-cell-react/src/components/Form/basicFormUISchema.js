1e77792b5b9cde3fa2cc13453d9b42bf35f0e05e
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-09-21 18:20:16 -0500

    updates

--- ai-single-cell-ui/ai-single-cell-react/src/components/Form/basicFormUISchema.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/Form/basicFormUISchema.js
@@ -1,67 +1,3 @@
-// import MyCreatableSelect from "./Components/Creatable";
-// const uiSchema = {
-//         "Task": {
-//           "ui:placeholder": "Select an option",
-//           'ui:widget': () => (
-//             <div className='common-row-wrap'>
-//               <MyCreatableSelect fieldName="task"/>
-//             </div>
-//           ),
-//         },
-//         "Author": {
-//             "ui:placeholder": "Select an option",
-//             'ui:widget': () => (
-//               <div className='common-row-wrap'>
-//                 <MyCreatableSelect fieldName="author" />
-//               </div>
-//             ),
-//         },
-//         "Species": {
-//             "ui:placeholder": "Select an option",
-//             'ui:widget': () => (
-//               <div className='common-row-wrap'>
-//                 <MyCreatableSelect fieldName="species" />
-//               </div>
-//             ),
-//         },
-//         "Organ Part": {
-//             "ui:placeholder": "Select an option",
-//             'ui:widget': () => (
-//               <div className='common-row-wrap'>
-//                 <MyCreatableSelect fieldName="organ-part" />
-//               </div>
-//             ),
-//           },
-//           "Selected Cell Types": {
-//             "ui:placeholder": "Select an option",
-//             'ui:widget': () => (
-//               <div className='common-row-wrap'>
-//                 <MyCreatableSelect fieldName="selected-cell-types" />
-//               </div>
-//             ),
-//           },
-//           "Disease Status (Donor)": {
-//             "ui:placeholder": "Select an option",
-//             'ui:widget': () => (
-//               <div className='common-row-wrap'>
-//                 <MyCreatableSelect fieldName="disease-status-donor" />
-//               </div>
-//             ),
-//           },
-//           "Cell Count Estimate": {
-//             "ui:placeholder": "Select an option",
-//             'ui:widget': () => (
-//               <div className='common-row-wrap'>
-//                 <MyCreatableSelect fieldName="cell-count-estimate" />
-//               </div>
-//             ),
-//           },
-//   };
-  
-
-//   export default uiSchema;
-
-
   // uiSchema.js
 
 import MyCreatableSelect from "./Components/Creatable";
@@ -117,11 +53,75 @@ const createUISchema = (commonOptions) => {
           </div>
         ),
       },
+      "Disease Status (Specimen)": {
+        "ui:placeholder": "Select an option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Disease Status (Specimen)" options={commonOptions} />
+          </div>
+        ),
+      },
       "Cell Count Estimate": {
         "ui:placeholder": "Select an option",
         'ui:widget': () => (
           <div className='common-row-wrap'>
             <MyCreatableSelect fieldName="Cell Count Estimate" options={commonOptions}/>
+          </div>
+        ),
+      },
+      "Sample Type": {
+        "ui:placeholder": "Select an option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Sample Type" options={commonOptions}/>
+          </div>
+        ),
+      },
+      "Anatomical Entity": {
+        "ui:placeholder": "Select an option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Anatomical Entity" options={commonOptions}/>
+          </div>
+        ),
+      },
+      "Model Organ": {
+        "ui:placeholder": "Select an option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Model Organ" options={commonOptions}/>
+          </div>
+        ),
+      },
+      "Library Construction Method": {
+        "ui:placeholder": "Select an option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Library Construction Method" options={commonOptions}/>
+          </div>
+        ),
+      },
+      "Nucleic Acid Source": {
+        "ui:placeholder": "Select an option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Nucleic Acid Source" options={commonOptions}/>
+          </div>
+        ),
+      },
+      "Development Stage": {
+        "ui:placeholder": "Select an option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Development Stage" options={commonOptions}/>
+          </div>
+        ),
+      },
+      "Source": {
+        "ui:placeholder": "Select an option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Source" options={commonOptions}/>
           </div>
         ),
       },
