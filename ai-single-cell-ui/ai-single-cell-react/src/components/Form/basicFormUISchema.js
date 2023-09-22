f7ba81777b4d3ea6e6541265de36c1d9d9272da1
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-09-22 13:17:44 -0500

    updates

--- ai-single-cell-ui/ai-single-cell-react/src/components/Form/basicFormUISchema.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/Form/basicFormUISchema.js
@@ -1,29 +1,59 @@
-//  // uiSchema.js
-
-//  import MyCreatableSelect from "./Components/Creatable";
-
-//  // Define the function that takes commonOptions as an argument
-//  const createUISchema = (commonOptions) => {
-//    return {
-//      "parameters": {
-//          "dataset": {
-//            "ui:widget": "textarea",
-//            "ui:placeholder": "Enter Text"
-//          },
-//          "submission_date": {
-//            "classNames": "sub-category",
-//          }
-//      }
-//    };
-//  };
- 
-//  export default createUISchema;
-
 export const uiSchema = {
   "parameters": {
-      "dataset": {
-        "ui:widget": "textarea",
-        "ui:placeholder": "Enter Text"
+    "classNames": "category",
+      "output_format": {
+        "classNames": "sub-category",
+        "ui:widget": "select",
+        "ui:placeholder": "Select file format"
+      },
+      "methods": {
+        "classNames": "sub-category",
+        // "ui:widget": "select",
+        "ui:placeholder": "Select a method",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <select>
+              <option value="CIDR">CIDR</option>
+            </select>
+      </div>
+        ),
+      },
+      "default_assay": {
+        "classNames": "sub-category",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <span data-v-22825496="" class="ui-form-title-message warning"> * Optional </span>
+            <input type='text' />
+      </div>
+        ),
+      },
+      "layer": {
+        "classNames": "sub-category"
+      },
+      "path_of_scrublet_calls": {
+        "classNames": "sub-category"
+      },
+      "species": {
+        "classNames": "sub-category",
+        "ui:placeholder": "Select species type"
+      },
+      "idtype": {
+        "classNames": "sub-category"
+      },
+      "genes": {
+        "classNames": "sub-category",
+      },
+      "ncores": {
+        "classNames": "sub-category",
+        "ui:widget": "range",
+      },
+      "show_umap": {
+        "classNames": "sub-category",
+        "ui:widget": "toggle"
+      },
+      "show_error": {
+        "classNames": "sub-category",
+        "ui:widget": "toggle"
       }
   }
 };
