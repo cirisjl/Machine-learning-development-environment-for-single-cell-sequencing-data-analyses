b23e8a455548da311ee68c2cb9b17eeb40404910
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-09-22 13:00:08 -0500

    updates

--- ai-single-cell-ui/ai-single-cell-react/src/components/Form/basicFormUISchema.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/Form/basicFormUISchema.js
@@ -1,20 +1,32 @@
- // uiSchema.js
+//  // uiSchema.js
 
- import MyCreatableSelect from "./Components/Creatable";
+//  import MyCreatableSelect from "./Components/Creatable";
 
- // Define the function that takes commonOptions as an argument
- const createUISchema = (commonOptions) => {
-   return {
-     "parameters": {
-         "dataset": {
-           "ui:widget": "textarea",
-           "ui:placeholder": "Enter Text"
-         },
-         "submission_date": {
-           "classNames": "sub-category",
-         }
-     }
-   };
- };
+//  // Define the function that takes commonOptions as an argument
+//  const createUISchema = (commonOptions) => {
+//    return {
+//      "parameters": {
+//          "dataset": {
+//            "ui:widget": "textarea",
+//            "ui:placeholder": "Enter Text"
+//          },
+//          "submission_date": {
+//            "classNames": "sub-category",
+//          }
+//      }
+//    };
+//  };
  
- export default createUISchema;
+//  export default createUISchema;
+
+export const uiSchema = {
+  "parameters": {
+      "dataset": {
+        "ui:widget": "textarea",
+        "ui:placeholder": "Enter Text"
+      },
+      "submission_date": {
+        "classNames": "sub-category",
+      }
+  }
+};
