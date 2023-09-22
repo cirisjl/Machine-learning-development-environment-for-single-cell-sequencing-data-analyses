504e183fdfc761ceacfb0283b5f027e2db9c55aa
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-09-22 13:49:26 -0500

    updates

--- ai-single-cell-ui/ai-single-cell-react/src/schema/UI-schema/Tools/evaluation/basicFormUISchema.js
+++ ai-single-cell-ui/ai-single-cell-react/src/schema/UI-schema/Tools/evaluation/basicFormUISchema.js
@@ -1,57 +1,169 @@
-export const uiSchema = {
-  "parameters": {
-    "classNames": "category",
-      "output_format": {
-        "classNames": "sub-category",
-        "ui:widget": "select",
-        "ui:placeholder": "Select file format"
+  // uiSchema.js
+
+  import MyCreatableSelect from "./Components/Creatable";
+
+  // Define the function that takes commonOptions as an argument
+  
+  export const uiSchema = {
+      "Dataset": {
+        "ui:placeholder": "Enter the Dataset name"
       },
-      "methods": {
-        "classNames": "sub-category",
-        // "ui:widget": "select",
-        "ui:placeholder": "Select a method",
+      "Task": {
+        "ui:placeholder": "Select/Create an Option",
         'ui:widget': () => (
           <div className='common-row-wrap'>
-            <select>
-              <option value="CIDR">CIDR</option>
-            </select>
-      </div>
+            <MyCreatableSelect fieldName="Task" />
+          </div>
         ),
       },
-      "default_assay": {
-        "classNames": "sub-category",
+      "Downloads": {
+        "ui:placeholder": "http://"
+      },
+      "Title": {
+        "ui:placeholder": "Enter title"
+      },
+      "Author": {
+          "ui:placeholder": "Select/Create an Option",
+          'ui:widget': () => (
+            <div className='common-row-wrap'>
+              <MyCreatableSelect fieldName="Author" />
+            </div>
+          ),
+      },
+      "Reference (paper)": {
+        "ui:placeholder": "Enter Reference"
+      },
+      "Abstract": {
+        "ui:widget": "textarea"
+      },
+      "DOI": {
+        "ui:widget": "textarea"
+      },
+      "Species": {
+          "ui:placeholder": "Select/Create an Option",
+          'ui:widget': () => (
+            <div className='common-row-wrap'>
+              <MyCreatableSelect fieldName="Species" />
+            </div>
+          ),
+      },
+      "Sample Type": {
+        "ui:placeholder": "Select/Create an Option",
         'ui:widget': () => (
           <div className='common-row-wrap'>
-            <span data-v-22825496="" class="ui-form-title-message warning"> * Optional </span>
-            <input type='text' />
-      </div>
+            <MyCreatableSelect fieldName="Sample Type" />
+          </div>
         ),
       },
-      "layer": {
-        "classNames": "sub-category"
+      "Anatomical Entity": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Anatomical Entity"/>
+          </div>
+        ),
       },
-      "path_of_scrublet_calls": {
-        "classNames": "sub-category"
+      "Organ Part": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Organ Part"/>
+          </div>
+        ),
       },
-      "species": {
-        "classNames": "sub-category",
-        "ui:placeholder": "Select species type"
+      "Model Organ": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Model Organ"/>
+          </div>
+        ),
       },
-      "idtype": {
-        "classNames": "sub-category"
+      "Selected Cell Types": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Selected Cell Types"/>
+          </div>
+        ),
       },
-      "genes": {
-        "classNames": "sub-category",
+      
+      "Library Construction Method": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Library Construction Method"/>
+          </div>
+        ),
       },
-      "ncores": {
-        "classNames": "sub-category",
-        "ui:widget": "range",
+      
+      "Nucleic Acid Source": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Nucleic Acid Source"/>
+          </div>
+        ),
       },
-      "show_umap": {
-        "classNames": "sub-category"
+  
+      
+      "Paired End": {
+        "ui:widget": "radio" // could also be "select"
       },
-      "show_error": {
-        "classNames": "sub-category"
-      }
-  }
-};
+      
+      "Analysis Protocol": {
+        "ui:placeholder": "Enter ...",
+      },
+      "Disease Status (Specimen)": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Disease Status (Specimen)" />
+          </div>
+        ),
+      },
+      
+      "Disease Status (Donor)": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Disease Status (Donor)"/>
+          </div>
+        ),
+      },
+      
+      "Development Stage": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Development Stage"/>
+          </div>
+        ),
+      },
+      "Donor Count": {
+        "ui:placeholder": "Enter ...",
+      },
+      "Cell Count Estimate": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Cell Count Estimate"/>
+          </div>
+        ),
+      },
+      "Source": {
+        "ui:placeholder": "Select/Create an Option",
+        'ui:widget': () => (
+          <div className='common-row-wrap'>
+            <MyCreatableSelect fieldName="Source" />
+          </div>
+        ),
+      },
+      "Source Key": {
+        "ui:placeholder": "Enter ...",
+      },
+      "Submission Date": {
+        "className":"date"
+      },
+  };
+  
