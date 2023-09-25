f71fc3938fa644bd0c15708c5e1721d4b0a37eb4
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-09-25 13:25:52 -0500

    Update customComponent.js

--- ai-single-cell-ui/ai-single-cell-react/src/components/Form/Components/customComponent.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/Form/Components/customComponent.js
@@ -197,7 +197,7 @@ class MyForm extends Component {
       this.setState((prevState) => {
         const newOption = { value: inputValue, label: inputValue };
         const updatedOptions = { ...prevState.options };
-        const newOptions = {...prevState.newOptions};
+        const newOptions = [...prevState.newOptions];
         updatedOptions[fieldName] = [...(updatedOptions[fieldName] || []), newOption];
     
         const updatedFormData = {
