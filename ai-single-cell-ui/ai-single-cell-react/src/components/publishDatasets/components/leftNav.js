e4c4a350268c47e2e1a70a640afa48643f075175
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-09-27 16:43:55 -0500

    Adding a new route for publish Datasets

---
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/leftNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function LeftNav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Task 1</a></li>
+        <li><a href="#">Task 2</a></li>
+        {/* Add links for all tasks */}
+      </ul>
+    </nav>
+  );
+}
+
+export default LeftNav;
