5e80bad305207f0185f97e6dcecbf950048a7f50
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-10-03 18:15:39 -0500

    Correcting

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/getMetadataTask.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/getMetadataTask.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function GetMetaDataComponent() {
+function GetMetaDataComponent({ setTaskStatus }) {
+  const handleTaskCompletion = () => {
+    // Perform the necessary actions for completing Task 1
+    // For example, submit a form, validate input, etc.
+
+    // After Task 1 is successfully completed, update the task status
+    setTaskStatus((prevTaskStatus) => ({
+      ...prevTaskStatus,
+      4: true, // Mark Task 1 as completed
+    }));
+  };
+
   return (
     <div>
-        <p>Get MetaData Page</p>
+      {/* Task 1 content here */}
+      <button onClick={handleTaskCompletion}>GetMetaDataComponent button</button>
     </div>
   );
 }
--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/taskBuilderTask.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/taskBuilderTask.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 function TaskBuilderTaskComponent({ setTaskStatus }) {
-  const handleTask1Completion = () => {
+  const handleTaskCompletion = () => {
     // Perform the necessary actions for completing Task 1
     // For example, submit a form, validate input, etc.
 
     // After Task 1 is successfully completed, update the task status
     setTaskStatus((prevTaskStatus) => ({
       ...prevTaskStatus,
-      1: true, // Mark Task 1 as completed
+      5: true, // Mark Task 1 as completed
     }));
   };
 
   return (
     <div>
       {/* Task 1 content here */}
-      <button onClick={handleTask1Completion}>TaskBuilderTaskComponent button</button>
+      <button onClick={handleTaskCompletion}>TaskBuilderTaskComponent button</button>
     </div>
   );
 }
