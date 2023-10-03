628ca6d9ada1520717863f99e555ad1f76d3539f
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-10-03 18:08:59 -0500

    Adding tasks

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/mainContent.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/mainContent.js
@@ -3,13 +3,19 @@ import UploadDataTaskComponent from './uploadDataTask';
 import ValidationTaskComponent from './validationTask';
 import GetMetaDataComponent from './getMetadataTask';
 import ReviewTaskComponent from './reviewTask';
+import QualityControlTaskComponent from './qualityControlTask';
+import TaskBuilderTaskComponent from './taskBuilderTask';
+import BenchmarksTaskComponent from './benchmarksTask';
 
 function MiddleContent({ activeTask, setTaskStatus }) {
   const taskComponents = {
     1: <UploadDataTaskComponent setTaskStatus={setTaskStatus} />,
     2: <ValidationTaskComponent setTaskStatus={setTaskStatus} />,
-    3: <GetMetaDataComponent setTaskStatus={setTaskStatus} />,
-    4: <ReviewTaskComponent setTaskStatus={setTaskStatus} />,
+    3: <QualityControlTaskComponent setTaskStatus={setTaskStatus} />,
+    4: <GetMetaDataComponent setTaskStatus={setTaskStatus} />,
+    5: <TaskBuilderTaskComponent setTaskStatus={setTaskStatus} />,
+    6: <BenchmarksTaskComponent setTaskStatus={setTaskStatus} />,
+    7: <ReviewTaskComponent setTaskStatus={setTaskStatus} />,
     // Add other task components here
   };
 
--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/validationTask.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/validationTask.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function ValidationTaskComponent() {
+function ValidationTaskComponent({ setTaskStatus }) {
+  const handleTask1Completion = () => {
+    // Perform the necessary actions for completing Task 1
+    // For example, submit a form, validate input, etc.
+
+    // After Task 1 is successfully completed, update the task status
+    setTaskStatus((prevTaskStatus) => ({
+      ...prevTaskStatus,
+      1: true, // Mark Task 1 as completed
+    }));
+  };
+
   return (
     <div>
-        <p>ValidationTaskComponent</p>
+      {/* Task 1 content here */}
+      <button onClick={handleTask1Completion}>ValidationTaskComponent</button>
     </div>
   );
 }
