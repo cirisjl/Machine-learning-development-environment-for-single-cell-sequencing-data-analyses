21185f730d55b7df1b3f84e5286bd67361e6bb8a
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-10-11 18:28:06 -0500

    Updates

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/getMetadataTask.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/getMetadataTask.js
@@ -20,12 +20,18 @@ function GetMetaDataComponent({ setTaskStatus, taskData, setTaskData, setActiveT
       {/* Task 1 content here */}
       <button onClick={handleTaskCompletion}>GetMetaDataComponent button</button>
 
-      <div className='previous'>
-        <button type="submit" class="btn btn-info" onClick={() => setActiveTask(activeTask - 1)} >Previous</button>
-      </div>
-      <div className='next-upon-success'>
-        <button type="submit" class="btn btn-info" onClick={handleTaskCompletion} >Next</button>
-      </div>
+      <div className='navigation-buttons'>
+            <div className="previous">
+              <button type="submit" className="btn btn-info button" onClick={() => setActiveTask(activeTask - 1)}>
+                Previous
+              </button>
+            </div>
+            <div className="next-upon-success">
+              <button type="submit" className="btn btn-info button" onClick={handleTaskCompletion}>
+                Next
+              </button>
+            </div>
+          </div>
 
     </div>
   );
