9a6debb7479dba628a54dde801abfbf7cabbf2d8
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-10-11 15:34:01 -0500

    Update validationTask.js

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/validationTask.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/validationTask.js
@@ -1,57 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { FLASK_BACKEND_API, STORAGE } from '../../../constants/declarations';
-import axios from 'axios';
 import { getCookie, isUserAuth } from '../../../utils/utilFunctions';
 import { useNavigate } from 'react-router-dom';
 import Select from 'react-select';
-import debounce from 'lodash/debounce';
 import { PropagateLoader } from 'react-spinners';
 
+function ValidationTaskComponent({ setTaskStatus, taskData, setTaskData, setActiveTask, activeTask }) {
+  const [seuratFiles, setSeuratFiles] = useState([]);
+  const [selectedSeuratFile, setSelectedSeuratFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [assayNamesMap, setAssayNamesMap] = useState({}); // Store fetched assay names
 
-function ValidationTaskComponent({ setTaskStatus, taskData, setTaskData, setActiveTask , activeTask}) {
-  const [assayNames, setAssayNames] = useState([]);
-  const [selectedAssays, setSelectedAssays] = useState([]);
-  const [seuratFiles, setSeuratFiles] = useState([]); // List to store Seurat files
-  const [selectedSeuratFile, setSelectedSeuratFile] = useState(null); // Selected Seurat file
-
-  // const [adataPaths, setAdataPaths] = useState({});
-  const [isFetchingData, setIsFetchingData] = useState(false);
-  const [loading, setLoading] = useState(false); // Initialize loading to true
-
-  let jwtToken = getCookie('jwtToken');
+  const jwtToken = getCookie('jwtToken');
   const navigate = useNavigate();
 
+  const fetchAssayNames = async (path, file) => {
+    if (!assayNamesMap[file]) {
+      setLoading(true);
 
-  const fetchAssayNames = debounce(async (path) => {
-    setIsFetchingData(true);
-    setLoading(true);
-    try {
-      const assayNamesPromises = seuratFiles.map((fileInfo) => {
-        return fetch(`${FLASK_BACKEND_API}/api/convert_to_anndata`, {
+      try {
+        const response = await fetch(`${FLASK_BACKEND_API}/api/convert_to_anndata`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ path: fileInfo.value }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            fileInfo.assayNames = data.assay_names.map((name) => ({ label: name, value: name }));
-            fileInfo.selectedAssays = [];
-            return fileInfo;
-          });
-      });
-      
+          body: JSON.stringify({ path }),
+        });
 
-      const filesWithAssayNames = await Promise.all(assayNamesPromises);
-      setSeuratFiles(filesWithAssayNames);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching assay names:', error);
-    } finally {
-      setIsFetchingData(false);
+        if (response.ok) {
+          const data = await response.json();
+          setAssayNamesMap((prevMap) => ({
+            ...prevMap,
+            [file]: data.assay_names.map((name) => ({ label: name, value: name })),
+          }));
+        } else {
+          console.error('Error fetching assay names:', response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching assay names:', error);
+      } finally {
+        setLoading(false);
+      }
     }
-  }, 300); // Debounce for 300 milliseconds
+  };
 
   useEffect(() => {
     isUserAuth(jwtToken)
@@ -60,20 +51,24 @@ function ValidationTaskComponent({ setTaskStatus, taskData, setTaskData, setActi
           let username = authData.username;
           let newDirectoryPath = taskData.upload.newDirectoryPath;
           let files = taskData.upload.files;
+          const updatedValidation = { ...taskData.validation };
+
           for (let file of files) {
             let path = STORAGE + "/" + username + "/" + newDirectoryPath + "/" + file;
 
             if (file.endsWith('.h5Seurat') || file.endsWith('.h5seurat') || file.endsWith('.rds')) {
               setSeuratFiles((seuratFiles) => [
                 ...seuratFiles,
-                { label: file, value: path, assayNames: [], selectedAssays: [] },
+                { label: file, value: path, assayNames: assayNamesMap[file] || [], selectedAssays: [] },
               ]);
+
+              // Store file paths in the taskData state under validation attribute
+              updatedValidation[file] = path;
             }
-            
           }
 
-          // Fetch assay names for Seurat files
-          fetchAssayNames();
+          // Update taskData with the new validation object
+          setTaskData({ ...taskData, validation: updatedValidation });
         } else {
           console.warn("Unauthorized - you must be an admin to access this page");
           navigate("/accessDenied");
@@ -82,21 +77,7 @@ function ValidationTaskComponent({ setTaskStatus, taskData, setTaskData, setActi
       .catch((error) => {
         console.error(error);
       });
-  }, []);
-
-
-    // // When the user selects a Seurat file, fetch assay names for that file
-    // useEffect(() => {
-    //   if (selectedSeuratFile) {
-    //     fetchAssayNames(selectedSeuratFile.value);
-    //   }
-    // }, [selectedSeuratFile]);
-  
-
-  const handleSelectionChange = (selectedOptions) => {
-    setSelectedAssays(selectedOptions.map((option) => option.value));
-  };
-  
+  }, [jwtToken, taskData, navigate]);
 
   const handleTaskCompletion = () => {
     // Perform the necessary actions for completing Task 1
@@ -107,48 +88,27 @@ function ValidationTaskComponent({ setTaskStatus, taskData, setTaskData, setActi
       ...prevTaskStatus,
       2: true, // Mark Task 1 as completed
     }));
-    //The current task is finished, so make the next task active
+    // The current task is finished, so make the next task active
     setActiveTask(3);
   };
 
-  // return (
-  //   <div>
-  //           {loading ? (
-  //             // Render the loading spinner while loading is true
-  //             <div className="spinner-container">
-  //             <PropagateLoader color={'#36D7B7'} loading={loading} size={15} />
-  //           </div>             
-  //           ) : (
-  //             <div>
-  //               <div>
-  //                 <h1>Select a Seurat File</h1>
-  //                 <Select
-  //                   options={seuratFiles}
-  //                   value={selectedSeuratFile}
-  //                   onChange={(selectedOption) => setSelectedSeuratFile(selectedOption)}
-  //                 />
-  //                 {selectedSeuratFile && (
-  //                   <div>
-  //                     <h1>Choose Assay Names</h1>
-  //                     <Select
-  //                       isMulti
-  //                       options={assayNames}
-  //                       value={assayNames.filter((option) => selectedAssays.includes(option.value))}
-  //                       onChange={handleSelectionChange}
-  //                     />
-  //                   </div>
-  //                 )}
-  //               </div>
-  //               <div className='previous'>
-  //                 <button type="submit" class="btn btn-info" onClick={() => setActiveTask(activeTask - 1)} >Previous</button>
-  //               </div>
-  //               <div className='next-upon-success'>
-  //                 <button type="submit" class="btn btn-info" onClick={handleTaskCompletion} >Next</button>
-  //               </div>
-  //             </div>
-  //         )}
-  //   </div>
-  // );
+  const handleSeuratFileChange = (selectedOption) => {
+    setSelectedSeuratFile(selectedOption);
+
+    // Fetch assay names when a Seurat file is selected
+    if (selectedOption) {
+      fetchAssayNames(selectedOption.value, selectedOption.label);
+    }
+  };
+
+  const handleAssayNamesChange = (selectedOptions) => {
+    if (selectedSeuratFile) {
+      const updatedSeuratFiles = [...seuratFiles];
+      const index = updatedSeuratFiles.findIndex((file) => file.value === selectedSeuratFile.value);
+      updatedSeuratFiles[index].selectedAssays = selectedOptions;
+      setSeuratFiles(updatedSeuratFiles);
+    }
+  };
 
   return (
     <div>
@@ -163,30 +123,29 @@ function ValidationTaskComponent({ setTaskStatus, taskData, setTaskData, setActi
             <Select
               options={seuratFiles}
               value={selectedSeuratFile}
-              onChange={(selectedOption) => setSelectedSeuratFile(selectedOption)}
+              onChange={handleSeuratFileChange}
             />
             {selectedSeuratFile && (
               <>
                 <h1>Choose Assay Names</h1>
                 <Select
                   isMulti
-                  options={selectedSeuratFile.assayNames}
+                  options={assayNamesMap[selectedSeuratFile.label] || []}
                   value={selectedSeuratFile.selectedAssays}
-                  onChange={(selectedOptions) => {
-                    setSelectedSeuratFile((prevSeuratFile) => ({
-                      ...prevSeuratFile,
-                      selectedAssays: selectedOptions,
-                    }));
-                  }}
+                  onChange={handleAssayNamesChange}
                 />
               </>
             )}
           </div>
-          <div className='previous'>
-            <button type="submit" class="btn btn-info" onClick={() => setActiveTask(activeTask - 1)} >Previous</button>
+          <div className="previous">
+            <button type="submit" className="btn btn-info" onClick={() => setActiveTask(activeTask - 1)}>
+              Previous
+            </button>
           </div>
-          <div className='next-upon-success'>
-            <button type="submit" class="btn btn-info" onClick={handleTaskCompletion} >Next</button>
+          <div className="next-upon-success">
+            <button type="submit" className="btn btn-info" onClick={handleTaskCompletion}>
+              Next
+            </button>
           </div>
         </div>
       )}
