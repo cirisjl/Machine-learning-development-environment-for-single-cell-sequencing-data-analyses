1c2abc2fe5ecb730b567ba63f5e62df425cf7bb7
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-10-18 18:02:46 -0500

    Update dashboard.py

--- api/dash_app/dashboard.py
+++ api/dash_app/dashboard.py
@@ -1076,5 +1076,5 @@ def update_active_assay(selected_assay, available_assays_options):
         return None
 
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+# if __name__ == "__main__":
+#     app.run_server(debug=True)
