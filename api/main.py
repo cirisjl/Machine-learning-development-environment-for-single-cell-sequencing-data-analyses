66d55f9a1a0b95e51df53096bd21b978bd200a88
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-10-17 19:17:10 -0500

    updates

--- api/main.py
+++ api/main.py
@@ -9,6 +9,7 @@ from routers import tools
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.wsgi import WSGIMiddleware
 from dash_app.dashboard import app as dashboard1
+from dashapp import create_dash_app
 
 
 def create_app() -> FastAPI:
@@ -45,11 +46,6 @@ async def add_process_time_header(request, call_next):
     response.headers["X-Process-Time"] = str(f'{process_time:0.4f} sec')
     return response
 
-# Define the main API endpoint
-@app.get("/index")
-def index():
-    return "Hello"
-
 
 @app.websocket("/taskStatus/{taskIdsCommaSeparated}")
 async def websocket_endpoint(websocket: WebSocket, taskIdsCommaSeparated: str):
@@ -69,6 +65,16 @@ async def websocket_endpoint(websocket: WebSocket, taskIdsCommaSeparated: str):
         await websocket.send_json(results)
         await asyncio.sleep(3)
 
+
+@app.get("/status")
+def get_status():
+    return {"status": "ok"}
+
+# A bit odd, but the only way I've been able to get prefixing of the Dash app
+# to work is by allowing the Dash/Flask app to prefix itself, then mounting
+# it to root
+dash_app = create_dash_app(requests_pathname_prefix="/dash/")
+app.mount("/dash", WSGIMiddleware(dash_app.server))
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=5000, reload=True)
