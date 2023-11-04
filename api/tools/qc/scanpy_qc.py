b85dfea6127ea279745154260cd31349b7eb71c1
cirisjl <cirisjl@gmail.com>
2023-11-04 12:50:41 -0400

    Add Cell Cycle for Scanpy, modified dropkick.

--- api/tools/qc/scanpy_qc.py
+++ api/tools/qc/scanpy_qc.py
@@ -12,21 +12,21 @@ sc.logging.print_header()
 sc.settings.set_figure_params(dpi=80, facecolor='white')
 
 
-def scanpy_qc(adata, min_genes=200, min_cells=3):
+def run_scanpy_qc(adata, min_genes=200, min_cells=3, target_sum=1e4, regress_cell_cycle=False):
         if adata is None:
             print("File format is not supported.")
             return None
 
         adata.var_names_make_unique() 
 
-        #Preprocessing
-        sc.pp.filter_cells(adata, min_genes)
-        sc.pp.filter_genes(adata, min_cells)
+        # Basic filtering
+        sc.pp.filter_cells(adata, min_genes=min_genes)
+        sc.pp.filter_genes(adata, min_cells=min_cells)
         adata.var['mt']=adata.var_names.str.startswith('MT-')  # Annotate the group of mitochondrial genes as 'mt'
         sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
         adata=adata[adata.obs.n_genes_by_counts < 2500, :]
         adata=adata[adata.obs.pct_counts_mt < 5, :]
-        sc.pp.normalize_total(adata, target_sum=1e4)
+        sc.pp.normalize_total(adata, target_sum=target_sum)
 
         sc.pp.log1p(adata)
 
@@ -37,11 +37,18 @@ def scanpy_qc(adata, min_genes=200, min_cells=3):
         sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
         sc.pp.scale(adata, max_value=10)
 
+        # Regress both S score and G2M score for cell cycle
+        if(regress_cell_cycle):
+             adata = regress_cell_cycle(adata)
+
         # Principal component analysis
         sc.tl.pca(adata, svd_solver='arpack')
 
         # Computing the neighborhood graph
         sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
+
+        # tSNE
+        sc.tl.tsne(adata)
 
         # Clustering the neighborhood graph
         sc.tl.umap(adata)
@@ -51,3 +58,31 @@ def scanpy_qc(adata, min_genes=200, min_cells=3):
 
         # return adata, output
         return adata
+
+
+def regress_cell_cycle(adata):
+    # Load cell cycle genes defined in Tirosh et al, 2015. It is a list of 97 genes, represented by their gene symbol.
+    cell_cycle_genes = [x.strip() for x in open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'regev_lab_cell_cycle_genes.txt')))]
+
+    # Define two lists, genes associated to the S phase and genes associated to the G2M phase
+    s_genes = cell_cycle_genes[:43]
+    g2m_genes = cell_cycle_genes[43:]
+    cell_cycle_genes = [x for x in cell_cycle_genes if x in adata.var_names]
+
+    # Perform cell cycle scoring. Cell cycle scoring adds three slots in data, a score for S phase, a score for G2M phase and the predicted cell cycle phase.
+    sc.tl.score_genes_cell_cycle(adata, s_genes=s_genes, g2m_genes=g2m_genes)
+
+    adata_cc_genes = adata[:, cell_cycle_genes]
+    sc.tl.pca(adata_cc_genes)
+    # sc.pl.pca_scatter(adata_cc_genes, color='phase')
+
+    # Regress out both S score and G2M score
+    sc.pp.regress_out(adata, ['S_score', 'G2M_score'])
+    sc.pp.scale(adata)
+
+    # Reproject dataset using cell cycle genes again. Since we regressed the scores, no effect of cell cycle is now evident.
+    adata_cc_genes = adata[:, cell_cycle_genes]
+    sc.tl.pca(adata_cc_genes)
+    # sc.pl.pca_scatter(adata_cc_genes, color='phase')
+
+    return adata
