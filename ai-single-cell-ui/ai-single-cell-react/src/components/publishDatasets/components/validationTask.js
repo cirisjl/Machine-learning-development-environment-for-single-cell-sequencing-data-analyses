b4e5d092794c4538b1ca094764aefdb77318656b
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-11-08 12:26:45 -0600

    Update validationTask.js

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/validationTask.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/validationTask.js
@@ -173,7 +173,7 @@ function ValidationTaskComponent({ setTaskStatus, taskData, setTaskData, setActi
             console.log("done iterating");
             console.log(hasAddedSeuratFiles)
 
-            if (!hasAddedSeuratFiles || taskData.validation.seuratFiles.length === 0) {
+            if (!hasAddedSeuratFiles) {
               console.log("done iterating if block");
 
                 // validation step is successful, move to next task as there are no seurat or rds datasets
