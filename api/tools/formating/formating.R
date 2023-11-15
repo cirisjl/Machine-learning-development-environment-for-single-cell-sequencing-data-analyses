9b892f93ee791a0a06b9625e39192c63cbbd9b0c
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-11-15 15:14:11 -0600

    Update formating.R

--- api/tools/formating/formating.R
+++ api/tools/formating/formating.R
@@ -369,7 +369,7 @@ SeuratToAnndata <- function(obj, out_file=NULL, assay="RNA", main_layer="counts"
     )
 
     if (!is.null(out_file)) {
-        adata$write(out_file, compression = "gzip")
+        write(adata, out_file, compression = "gzip")
         print("AnnData object is saved successfully.")
     }
 
