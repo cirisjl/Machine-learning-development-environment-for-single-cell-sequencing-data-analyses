26f66f6a45c3e01fca49065fdb09c89d4b3b078e
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-12-01 20:36:27 -0600

    Update conversion.py

--- api/routers/conversion.py
+++ api/routers/conversion.py
@@ -257,17 +257,17 @@ async def process_task_data(data: BenchmarksRequest):
                 # print("scvi")
                 # print(scvi_results)
 
-                # Call scvi_clustering method
-                asw_scvi, nmi_scvi, ari_scvi, time_points_scvi, cpu_usage_scvi, mem_usage_scvi, gpu_mem_usage_scanpy, gpu_mem_usage_scvi = seurat_clustering(adata_path, task_label)
+                # Call seurat_clustering method
+                asw_scvi_seurat, nmi_scvi_seurat, ari_scvi_seurat, time_points_seurat, cpu_usage_seurat, mem_usage_seurat, gpu_mem_usage_seurat = seurat_clustering(adata_path, task_label)
 
                 seurat_results = {
-                    "asw_score": asw_scvi,
-                    "nmi_score": nmi_scvi,
-                    "ari_score": ari_scvi,
-                    "time_points": time_points_scvi,
-                    "cpu_usage": cpu_usage_scvi,
-                    "mem_usage": mem_usage_scvi,
-                    "gpu_mem_usage": gpu_mem_usage_scvi
+                    "asw_score": asw_scvi_seurat,
+                    "nmi_score": nmi_scvi_seurat,
+                    "ari_score": ari_scvi_seurat,
+                    "time_points": time_points_seurat,
+                    "cpu_usage": cpu_usage_seurat,
+                    "mem_usage": mem_usage_seurat,
+                    "gpu_mem_usage": gpu_mem_usage_seurat
                 }
                 print("Seurat")
                 print(seurat_results)
@@ -276,7 +276,7 @@ async def process_task_data(data: BenchmarksRequest):
                 x_values = ['ARI', 'Silhouette', 'NMI']
                 y_values = {
                     'Scanpy': [ scanpy_results['ari_score'], scanpy_results['asw_score'], scanpy_results['nmi_score']],
-                    # 'scVI': [scvi_results['ari_score'], scvi_results['asw_score'], scvi_results['nmi_score']],
+                    'Seurat': [seurat_results['ari_score'], seurat_results['asw_score'], seurat_results['nmi_score']],
                 }
 
                 # # Call the plot_bar function
@@ -287,9 +287,9 @@ async def process_task_data(data: BenchmarksRequest):
                     'Scanpy_CPU': scanpy_results['cpu_usage'],
                     'Scanpy_Memory': scanpy_results['mem_usage'],
                     'Scanpy_GPU': scanpy_results['gpu_mem_usage'],
-                    # 'scVI_CPU': scvi_results['cpu_usage'],
-                    # 'scVI_Memory': scvi_results['mem_usage'],
-                    # 'scVI_GPU': scvi_results['gpu_mem_usage'],
+                    'Seurat_CPU': seurat_results['cpu_usage'],
+                    'Seurat_Memory': seurat_results['mem_usage'],
+                    'Seurat_GPU': seurat_results['gpu_mem_usage'],
                 }
 
                 # Call the plot_line function with an empty array for x
