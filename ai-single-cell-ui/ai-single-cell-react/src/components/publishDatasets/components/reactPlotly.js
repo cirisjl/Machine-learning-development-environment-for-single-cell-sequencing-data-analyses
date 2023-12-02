95f2810cd0610e17ec5f936057a05289044819cb
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-12-02 01:03:05 -0600

    Update reactPlotly.js

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/reactPlotly.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/reactPlotly.js
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from 'react';
 import Plot from 'react-plotly.js';
 
 function ReactPlotly({plot_data}) {
-  const [plotData, setPlotData] = useState(null);
-
-  useEffect(() => {
-    if (plot_data) {
-      setPlotData(JSON.parse(plot_data));
-    }
-  }, [plot_data]);
 
   return (
     <div>
-      {plotData && <Plot data={plotData.data} layout={plotData.layout} />}
+      {plot_data && <Plot data={JSON.parse(plot_data).data} layout={JSON.parse(plot_data).layout} />}
     </div>
   );
 }
