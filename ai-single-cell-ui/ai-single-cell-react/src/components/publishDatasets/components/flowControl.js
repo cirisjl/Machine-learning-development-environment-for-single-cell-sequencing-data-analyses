16507d93232a872bfcbe41209f9db15028e3f3de
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-12-20 06:17:26 -0600

    Create flowControl.js

---
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/flowControl.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import PublishDataset from '../publishDataset';
+
+const FlowControl = () => {
+
+  const [flow, setFlow] = useState('');
+
+
+  const startFromBeginning = () => {
+    setFlow('upload');
+  };
+
+  const startFromFourthStep = () => {
+    setFlow('taskBuilder');
+  };
+
+  return (
+    <div>
+      <h3>If you want to create a new dataset and then build tasks, click on "Create a New Dataset". If you want to use existing datasets to build tasks, click on "Jump to Task Builder".</h3>
+      <button onClick={startFromBeginning}>Create a new Dataset</button>
+      <button onClick={startFromFourthStep}>Jump to Task builder</button>
+      
+      <div>
+        {flow === 'upload' && <div><PublishDataset /></div>}
+        {flow === 'taskBuilder' && <div>Step 4 Component or Content</div>}
+      </div>
+    </div>
+  );
+};
+
+export default FlowControl;
