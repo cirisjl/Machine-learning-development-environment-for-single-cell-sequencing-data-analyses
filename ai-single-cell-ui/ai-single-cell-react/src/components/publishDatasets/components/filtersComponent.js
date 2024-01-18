b7a785e9f05368be642bdcc74e7d7857ec67c261
karthikgou <42573704+karthikgou@users.noreply.github.com>
2024-01-18 07:02:09 -0600

    adding styles

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/filtersComponent.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/filtersComponent.js
@@ -17,22 +17,42 @@ const FilterComponent = ({ name, options, activeFilters, onFilterChange, isVisib
                 <p onClick={() => onCategoryChange(name)}>{name}</p>
             </div>
             {isVisible && (
-                <div className='filter options'>
-                    <ul>
-                        {options.map(option => (
-                            <li key={option._id}>
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        checked={activeFilters[name]?.includes(option._id)}
-                                        onChange={() => onFilterChange(name, option._id)}
-                                    />
-                                     {option._id} ({option.count})
-                                </label>
-                            </li>
-                        ))}
-                    </ul>
-                    <button className="apply-filters-button">Apply</button>
+                <div className='filters-box-searchable'>
+                    <div className='facet-container'>
+                        <div className='single-facet'>
+                            <div className='filters-box-searchable'>
+                                <div className='filters-searchbar'>
+                                    <input autocomplete="false" placeholder="Search for a filter" type="text" id="filters-search-bar-filters-box-searchable-organ" class="form-control" value="" fdprocessedid="vdtrz3"/>
+                                </div>
+
+                                <div className='filters-header'>
+                                    <p>Available filters:</p>
+                                </div>
+
+                                <div className='filters-options'>
+                                    <ul>
+                                        {options.map(option => (
+                                            <li key={option._id}>
+                                                <label>
+                                                    <input
+                                                        type="checkbox"
+                                                        checked={activeFilters[name]?.includes(option._id)}
+                                                        onChange={() => onFilterChange(name, option._id)}
+                                                    />
+                                                    {option._id} ({option.count})
+                                                </label>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='filters-footer'>
+                        <button className="apply-filters-button">Apply</button>
+                    </div>
                 </div>
             )}
         </div>
