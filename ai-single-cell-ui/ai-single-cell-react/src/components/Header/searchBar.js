18062d44bbe8c4ab980586ad73392e9e8a993fc5
karthikgou <42573704+karthikgou@users.noreply.github.com>
2024-01-18 09:12:42 -0600

    updates

--- ai-single-cell-ui/ai-single-cell-react/src/components/Header/searchBar.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/Header/searchBar.js
@@ -31,7 +31,7 @@ function SearchBox({ onSearch, placeHolder }) {
         onChange={handleInputChange}
       />
       
-      <svg className="absolute left-2.5 text-gray-400 top-1/2 transform -translate-y-1/2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+      <svg className="absolute left-2.5 right text-gray-400 top-1/2 transform -translate-y-1/2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
         <path d="M30 28.59L22.45 21A11 11 0 1 0 21 22.45L28.59 30zM5 14a9 9 0 1 1 9 9a9 9 0 0 1-9-9z" fill="currentColor"></path>
       </svg>    
 
