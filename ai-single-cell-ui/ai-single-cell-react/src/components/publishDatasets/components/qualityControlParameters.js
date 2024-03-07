97c1dd54cb05d0cb46893d6915c581cf737c2817
U15EC104 <U15EC104@localhost>
2024-03-07 17:45:07 -0500

    Initial commit

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/qualityControlParameters.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/qualityControlParameters.js
@@ -7,23 +7,10 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 import {ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Button } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-const defaultValues = {
-    min_genes: 200,
-    max_genes: 20000, // No limit
-    min_cells: 2,
-    target_sum: 1e4,
-    n_top_genes: 2000,
-    n_neighbors: 15,
-    n_pcs: 0, // None
-    resolution: 1,
-    regress_cell_cycle: false,
-    use_default: true,
-  };
-
-const QualityControlParameters = () => {
-  const [values, setValues] = useState(defaultValues);
+const QualityControlParameters = ({values, setValues, defaultValues}) => {
 
   const handleSliderChange = (event) => {
     const { name, value } = event.target;
@@ -33,6 +20,31 @@ const QualityControlParameters = () => {
       use_default: false,
     }));
   };
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      marginBottom: theme.spacing(2), // Adds space between expansion panels
+    },
+    panelSummary: {
+      fontSize: '14px', // Adjusts font size for panel titles
+      '& .MuiExpansionPanelSummary-content': {
+        margin: theme.spacing(1), // Optionally adjust spacing if needed
+      },
+    },
+    panelDetails: {
+      display: 'block',
+      fontSize: '12px', // Adjusts font size for panel content
+    },
+    formGroup: {
+      margin: theme.spacing(2), 
+    },
+    formControlLabel: { // Optionally if you want to target labels specifically
+      '& .MuiTypography-root': {
+        fontSize: '12px', // Adjusts font size for form control labels
+      },
+    },
+  }));
 
   const handleSwitchChange = (event) => {
     const { name, checked } = event.target;
@@ -63,13 +75,13 @@ const QualityControlParameters = () => {
         Advanced Quality Control Parameters
       </Typography>
       
-      <ExpansionPanel>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+      <ExpansionPanel className={useStyles.root}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} className={useStyles.panelSummary}>
             {/* QC Parameters */}
             <Typography variant="h6" gutterBottom>QC Parameters</Typography>
         </ExpansionPanelSummary>
-        <ExpansionPanelDetails style={{ display: 'block' }}>
-             <FormGroup>
+        <ExpansionPanelDetails className={useStyles.panelDetails}>
+             <FormGroup className={useStyles.formGroup}>
                 <Box sx={{ m: 2 }}>
                     <Typography gutterBottom>Min Genes - Max Genes: [{values.min_genes} - {values.max_genes}]</Typography>
                     <Slider
@@ -80,6 +92,7 @@ const QualityControlParameters = () => {
                     max={20000}
                     step={25}
                     name="min_max_genes"
+                    marks={[{}]}
                     />
                 </Box>
 
@@ -126,10 +139,39 @@ const QualityControlParameters = () => {
                 </Box>
 
                 <Box sx={{ m: 2 }}>
+                  <Typography gutterBottom>
+                    Doublet Rate: {`${(values.doublet_rate * 100).toFixed(2)}%`}
+                  </Typography>
+                  <Slider
+                    value={values.doublet_rate}
+                    min={0}
+                    max={0.5}
+                    step={0.001}
+                    onChange={(e, val) => handleSliderChange({ target: { name: 'doublet_rate', value: val } })}
+                    valueLabelDisplay="auto"
+                    valueLabelFormat={(value) => `${(value * 100).toFixed(2)}%`}
+                    marks={[
+                      { value: 0, label: '0%' },
+                      { value: 0.008, label: '0.8%' },
+                      { value: 0.023, label: '2.3%' },
+                      { value: 0.038, label: '3.8%' },
+                      { value: 0.046, label: '4.6%' },
+                      { value: 0.061, label: '6.1%' },
+                      { value: 0.08, label: '8%*' }, // Default
+                      { value: 0.125, label: '12.5%' },
+                      { value: 0.2, label: '20%' },
+                      { value: 0.5, label: '50%' },
+                    ]}
+                    name="doublet_rate"
+                  />
+                </Box>
+
+                <Box sx={{ m: 2 }}>
 
                     <FormControlLabel
                         control={<Switch checked={values.regress_cell_cycle} onChange={handleSwitchChange} name="regress_cell_cycle" />}
                         label={`Regress Cell Cycle: ${values.regress_cell_cycle ? 'Yes' : 'No'}`}
+                        className={useStyles.formControlLabel}
                     />
                 </Box>
             </FormGroup>
@@ -137,14 +179,14 @@ const QualityControlParameters = () => {
       </ExpansionPanel>
 
 
-      <ExpansionPanel>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+      <ExpansionPanel className={useStyles.root}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} className={useStyles.panelSummary}>
             {/* Projection Parameters */}
             <Typography variant="h6" gutterBottom>Projection Parameters</Typography>
         </ExpansionPanelSummary>
         
-        <ExpansionPanelDetails style={{ display: 'block' }}>
-            <FormGroup>
+        <ExpansionPanelDetails className={useStyles.panelDetails}>
+            <FormGroup className={useStyles.formGroup}>
                 <Box sx={{ m: 2 }}>
                     <Typography gutterBottom>n_neighbors: {values.n_neighbors}</Typography>
                     <Slider
@@ -175,13 +217,13 @@ const QualityControlParameters = () => {
       </ExpansionPanel> 
 
 
-      <ExpansionPanel>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+      <ExpansionPanel className={useStyles.root}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} className={useStyles.panelSummary}>
           <Typography variant="h6" gutterBottom>Clustering Parameters</Typography>
         </ExpansionPanelSummary>
 
-        <ExpansionPanelDetails style={{ display: 'block' }}>
-            <FormGroup>
+        <ExpansionPanelDetails className={useStyles.panelDetails}>
+            <FormGroup className={useStyles.formGroup}>
 
                 <Box sx={{ m: 2 }}>
                     <Typography gutterBottom>Resolution: {values.resolution}</Typography>
@@ -203,8 +245,8 @@ const QualityControlParameters = () => {
         <FormControlLabel
             control={<Switch checked={values.use_default} onChange={handleSwitchChange} name="use_default" />}
             label="Use Default Values"
+            className={useStyles.formControlLabel}
         />
-
     </div>
   );
 };
--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/qualityControlTask.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/qualityControlTask.js
@@ -1,13 +1,116 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { CELERY_BACKEND_API} from '../../../constants/declarations';
+import { CELERY_BACKEND_API, STORAGE} from '../../../constants/declarations';
 import { ScaleLoader } from 'react-spinners';
 import ReactPlotly from './reactPlotly';
+import {isUserAuth, getCookie} from '../../../utils/utilFunctions';
+import QualityControlParameters from './qualityControlParameters';
+import {Button } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
+
+const defaultValues = {
+  min_genes: 200,
+  max_genes: 20000, // No limit
+  min_cells: 2,
+  target_sum: 1e4,
+  n_top_genes: 2000,
+  n_neighbors: 15,
+  n_pcs: 0, // None
+  resolution: 1,
+  regress_cell_cycle: false,
+  use_default: true,
+  doublet_rate: 0.08
+};
 
 function QualityControlTaskComponent({ setTaskStatus, taskData, setTaskData, setActiveTask, activeTask  }) {
   
   const [loading, setLoading] = useState(false);
+  const [values, setValues] = useState(defaultValues);
+  const navigate = useNavigate();
+
+  const runQualityControl = async() => {
+
+    setLoading(true);
+
+    let file_paths = taskData.quality_control.file_paths;
+    let pathToUse;
+  
+    if (file_paths.length > 1) {
+        const firstFilePath = file_paths[0]; 
+        pathToUse = firstFilePath.substring(0, firstFilePath.lastIndexOf('/'));
+    } else if (file_paths.length === 1) {
+        // Only one file, use its complete path
+        pathToUse = file_paths[0];
+    }
+
+    let inputRequest = {
+      fileDetails: pathToUse,
+      min_genes : values.min_genes,
+      max_genes : values.max_genes,
+      min_cells : values.min_cells,
+      target_sum : values.target_sum,
+      n_top_genes : values.n_top_genes,
+      n_neighbors : values.n_neighbors,
+      n_pcs : values.n_pcs,
+      resolution : values.resolution,
+      regress_cell_cycle : values.regress_cell_cycle,
+      use_default : values.use_default,
+      doublet_rate: values.doublet_rate
+    }
+
+    try {
+      const response = await axios.post(`${CELERY_BACKEND_API}/convert/publishDatasets/run/quality_control`, inputRequest);
+      const qualityControlResults = response.data;
+          
+      // Update the qc_results state with the quality control results
+      setTaskData((prevTaskData) => ({
+        ...prevTaskData,
+        quality_control: {
+          ...prevTaskData.quality_control,
+          qc_results: qualityControlResults,
+        },
+      }));
+      setLoading(false);
+    } catch (error) {
+      console.error('There was a problem with the axios operation:', error.response ? error.response.data : error.message);
+      setLoading(false);
+    }
+
+  };
+
+
+  useEffect(() => {
+    isUserAuth(getCookie('jwtToken'))
+    .then((authData) => {
+      if (authData.isAdmin) {
+        if(taskData.quality_control.status !== 'completed') {
+          let files = taskData.upload.files;
+          let newDirectoryPath = taskData.upload.newDirectoryPath;
+
+          let inputFiles = [];
+          for (let file of files) {
+            let path = STORAGE + "/" + authData.username + "/" + newDirectoryPath + "/" + file;
+            inputFiles.push(path);
+          }
+          setTaskData((prevTaskData) => ({
+            ...prevTaskData,
+            quality_control: {
+              ...prevTaskData.quality_control,
+              file_paths: inputFiles,
+              token: authData.username
+            },
+          }));   
+        }
+      }  else {
+        console.warn("Unauthorized - you must be an admin to access this page");
+        navigate("/accessDenied");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }, []); 
 
   // useEffect(() => {
   //   if(taskData.quality_control.status !== 'completed') {
@@ -67,16 +170,24 @@ function QualityControlTaskComponent({ setTaskStatus, taskData, setTaskData, set
     // After Task 3 is successfully completed, update the task status
     setTaskStatus((prevTaskStatus) => ({
       ...prevTaskStatus,
-      3: true, // Mark Task 3 as completed
+      2: true, // Mark Task 3 as completed
     }));
 
     //The current task is finished, so make the next task active
-    setActiveTask(4);
+    setActiveTask(3);
   };
 
   return (
     <div className='quality-control-task'>
-      {/* Task 1 content here */}
+      <div>
+        <QualityControlParameters values={values} setValues={setValues} defaultValues={defaultValues}/>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+          <Button onClick={runQualityControl} variant="contained" color="primary">
+            Run Quality Control
+          </Button>
+        </div>
+      </div>
+
       {loading ? (
         <div className="spinner-container">
           <ScaleLoader color="#36d7b7" loading={loading} />
@@ -84,10 +195,6 @@ function QualityControlTaskComponent({ setTaskStatus, taskData, setTaskData, set
       ) : (
       <div>
         <div className="App">
-        {/* {taskData.quality_control.qc_results &&
-              taskData.quality_control.qc_results.map((result, index) => (
-                result.umap_plot && <ReactPlotly plot_data={result.umap_plot} />
-        ))} */}
         {taskData.quality_control.qc_results &&
           taskData.quality_control.qc_results.map((result, index) => (
             <React.Fragment key={index}>
