d73ed93bc27c6c812a03c0dc348e25c49b4f150d
U15EC104 <U15EC104@localhost>
2024-04-14 20:07:06 -0400

    Modifying the schema for all the tools

--- ai-single-cell-ui/ai-single-cell-react/src/schema/UI-schema/Tools/imputation/imputation_magic.js
+++ ai-single-cell-ui/ai-single-cell-react/src/schema/UI-schema/Tools/imputation/imputation_magic.js
@@ -1,60 +1,124 @@
 export const uiSchema = {
-    "parameters": {
-      "classNames": "category",
-        "output_format": {
-          "classNames": "sub-category",
-          "ui:widget": "select",
-          "ui:placeholder": "Select file format"
-        },
+  "parameters": {
+    "classNames": "category",
+      "output_format": {
+        "classNames": "sub-category",
+        "ui:widget": "SelectComponent",
+        'ui:options': {
+          clearable: true ,
+          placeholder: "Select the Output Format",
+          creatable: false,
+          searchable: true,
+          opts:["AnnData", "SingleCellExperiment", "Seurat", "CSV"] 
+        }
+      },
+      "species": {
+        "classNames": "sub-category",
+        "ui:widget": "SelectComponent",
+        'ui:options': {
+          clearable: true ,
+          placeholder: "Select the Species type",
+          creatable: false,
+          searchable: true,
+          opts:["human", "mouse"]
+        }
+      },
+      "idtype": {
+        "classNames": "sub-category",
+        "ui:widget": "SelectComponent",
+        'ui:options': {
+          clearable: true ,
+          placeholder: "Select the ID type",
+          creatable: false,
+          searchable: true,
+          opts:["SYMBOL", "ENSEMBL", "ENTREZID", "REFSEQ"]
+        }
+      },
+      "cluster_label": {
+        "classNames": "sub-category",
+        "ui:widget": "ClusterLabelInput"
+      },
+      "show_umap": {
+        "classNames": "sub-category",
+        "ui:widget": "toggle"
+      },
+      "show_error": {
+        "classNames": "sub-category",
+        "ui:widget": "toggle"
+      },
+      "imputation_params": {
+        "classNames": "form-subset sub-category",
         "methods": {
           "classNames": "sub-category",
-          // "ui:widget": "select",
-          "ui:placeholder": "Select a method",
-          'ui:widget': () => (
-            <div className='common-row-wrap'>
-              <select>
-                <option value="Magic">Magic</option>
-              </select>
-        </div>
-          ),
+          "ui:widget": "MultiSelectComponent",
         },
-        "default_assay": {
+        "assay": {
           "classNames": "sub-category",
-          'ui:widget': () => (
-            <div className='common-row-wrap'>
-              <span data-v-22825496="" class="ui-form-title-message warning"> * Optional </span>
-              <input type='text' />
-        </div>
-          ),
+          "ui:widget": "ClusterLabelInput"
         },
         "layer": {
-          "classNames": "sub-category"
-        },
-        "path_of_scrublet_calls": {
-          "classNames": "sub-category"
-        },
-        "species": {
           "classNames": "sub-category",
-          "ui:placeholder": "Select species type"
+          "ui:widget": "ClusterLabelInput"
         },
-        "idtype": {
-          "classNames": "sub-category"
-        },
-        "genes": {
+        "min_genes": {
           "classNames": "sub-category",
+          "ui:widget": "RangeSlider",
+          'ui:options': {
+            title: 'Min Genes: ', // Title for the slider
+            min: 0,
+            max: 20000,
+            step: 25,
+            marks: [
+              { value: 200, label: '200*' },
+              { value: 1000, label: '1000' },
+              { value: 5000, label: '5000' },
+              { value: 10000, label: '10000' },
+              { value: 15000, label: '15000' },
+              { value: 20000, label: '20000' }
+            ]
+          },
+          'ui:title': 'Min Genes', 
+        },
+        "max_genes": {
+          "classNames": "sub-category",
+          "ui:widget": "RangeSlider",
+          'ui:options': {
+            title: 'Max Genes: ', // Title for the slider
+            min: 0,
+            max: 20000,
+            step: 25,
+            marks: [
+              { value: 200, label: '200*' },
+              { value: 1000, label: '1000' },
+              { value: 5000, label: '5000' },
+              { value: 10000, label: '10000' },
+              { value: 15000, label: '15000' },
+              { value: 20000, label: '20000' }
+            ]
+          },
+          'ui:title': 'Max Genes', 
         },
         "ncores": {
           "classNames": "sub-category",
-          "ui:widget": "range",
-        },
-        "show_umap": {
-          "classNames": "sub-category",
-          "ui:widget": "toggle"
-        },
-        "show_error": {
-          "classNames": "sub-category",
-          "ui:widget": "toggle"
-        }
-    }
-  };
-  
+          "ui:widget": "RangeSlider",
+          'ui:title': 'ncores: ', 
+          'ui:options': {
+            title: 'ncores: ', 
+            min: 0,
+            max: 20,
+            step: 1,
+            marks:[
+              { value: 0, label: '0' },
+              { value: 5, label: '5' },
+              { value: 12, label: '12*' },
+              { value: 20, label: '20' }
+            ]
+          }
+        } 
+      },
+      "use_default": {
+        "classNames": "sub-category",
+        "ui:widget": "toggle"
+      }
+  }
+};
