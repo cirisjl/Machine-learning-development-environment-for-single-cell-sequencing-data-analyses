faead812a6f73f0ca9c3ec879ceabe1bdac8081b
U15EC104 <U15EC104@localhost>
2024-04-18 17:39:29 -0400

    Revert "Modifying the schema for all the tools"
    This reverts commit 7ec0d3e92aa2b27fd711538df3cc653bdb24b153.

--- ai-single-cell-ui/ai-single-cell-react/src/components/MyData/Tools/components/sliderComponent.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/MyData/Tools/components/sliderComponent.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
-import Box from '@mui/material/Box';
-
 
 const RangeSlider = ({ title, value, onChange,options }) => {
-  // const handleChange = (event, newValue) => {
-  //   onChange(newValue);
-  // };
+  const handleChange = (event, newValue) => {
+    onChange(newValue);
+  };
 
   const handleDragChange = (event, newValue) => {
-    onChange(newValue); 
+    onChange(newValue); // newValue will be an array [min, max]
   };
 
   return (
     <div>
-    <Box sx={{ m: 2 }}>
+      {/* <Typography gutterBottom>{options.title}: <b>{value}</b></Typography> */}
       <Slider
         value={value}
-        // onChange={handleChange}
-        onChangeCommitted={handleDragChange} 
+        onChange={handleChange}
+        onChangeCommitted={handleDragChange} // Use onChangeCommitted for smoother drag behavior
         valueLabelDisplay="auto"
         min={options.min}
         max={options.max}
         step={options.step}
         marks={options.marks}
       />
-      </Box>
     </div>
   );
 };
