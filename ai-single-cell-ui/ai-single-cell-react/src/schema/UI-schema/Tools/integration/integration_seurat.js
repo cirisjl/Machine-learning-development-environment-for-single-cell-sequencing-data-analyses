4a930afd3d3f7d061923a82b1944705ba59262a4
U15EC104 <U15EC104@localhost>
2024-04-18 17:38:10 -0400

    Revert "Updating frontend code to adjust to the backend code"
    This reverts commit abc0f2024f7d409ad82841a483ffd90e298aeb49.

--- ai-single-cell-ui/ai-single-cell-react/src/schema/UI-schema/Tools/integration/integration_seurat.js
+++ ai-single-cell-ui/ai-single-cell-react/src/schema/UI-schema/Tools/integration/integration_seurat.js
@@ -1,120 +1,60 @@
 export const uiSchema = {
-  "parameters": {
-    "classNames": "category",
-    "output_format": {
-      "classNames": "sub-category",
-      "ui:widget": "SelectComponent",
-      'ui:options': {
-        clearable: true ,
-        placeholder: "Select the Output Format",
-        creatable: false,
-        searchable: true,
-        opts:["AnnData", "SingleCellExperiment", "Seurat", "CSV"] 
-      }
-    },
-    "methods": {
-      "classNames": "sub-category",
-      "ui:widget": "MultiSelectComponent",
-    },
-    "params": {
-      "classNames": "form-subset sub-category",
-      "default_assay": {
-        "classNames": "sub-category",
-        "ui:widget": "ClusterLabelInput"
-      },
-      "npcs": {
-        "classNames": "sub-category",
-        "ui:widget": "RangeSlider",
-        'ui:title': 'n_pcs: ', 
-        'ui:options': {
-          title: 'n_pcs: ', 
-          min: 0,
-          max: 200,
-          step: 1,
-          marks:[
-            { value: 0, label: '0*' },
-            { value: 5, label: '5' },
-            { value: 10, label: '10' },
-            { value: 20, label: '20' },
-            { value: 40, label: '40' },
-            { value: 50, label: '50' },
-            { value: 125, label: '125' },
-            { value: 200, label: '200' },
-          ]
-        }
-      },
-      "layer": {
-        "classNames": "sub-category",
-        "ui:widget": "ClusterLabelInput"
-      },
-      "species": {
-        "classNames": "sub-category",
-        "ui:widget": "SelectComponent",
-        'ui:options': {
-          clearable: true ,
-          placeholder: "Select the Species type",
-          creatable: false,
-          searchable: true,
-          opts:["human", "mouse"]
-        }
-      },
-      "idtype": {
-        "classNames": "sub-category",
-        "ui:widget": "SelectComponent",
-        'ui:options': {
-          clearable: true ,
-          placeholder: "Select the ID type",
-          creatable: false,
-          searchable: true,
-          opts:["SYMBOL", "ENSEMBL", "ENTREZID", "REFSEQ"]
-        }
-      },
-      "min_genes": {
-        "classNames": "sub-category",
-        "ui:widget": "RangeSlider",
-        'ui:options': {
-          title: 'Min Genes: ', // Title for the slider
-          min: 0,
-          max: 20000,
-          step: 25,
-          marks: [
-            { value: 200, label: '200*' },
-            { value: 1000, label: '1000' },
-            { value: 5000, label: '5000' },
-            { value: 10000, label: '10000' },
-            { value: 15000, label: '15000' },
-            { value: 20000, label: '20000' }
-          ]
+    "parameters": {
+      "classNames": "category",
+        "output_format": {
+          "classNames": "sub-category",
+          "ui:widget": "select",
+          "ui:placeholder": "Select file format"
         },
-        'ui:title': 'Min Genes', 
-      },
-      "max_genes": {
-        "classNames": "sub-category",
-        "ui:widget": "RangeSlider",
-        'ui:options': {
-          title: 'Max Genes: ', // Title for the slider
-          min: 0,
-          max: 20000,
-          step: 25,
-          marks: [
-            { value: 200, label: '200*' },
-            { value: 1000, label: '1000' },
-            { value: 5000, label: '5000' },
-            { value: 10000, label: '10000' },
-            { value: 15000, label: '15000' },
-            { value: 20000, label: '20000' }
-          ]
+        "methods": {
+          "classNames": "sub-category",
+          // "ui:widget": "select",
+          "ui:placeholder": "Select a method",
+          'ui:widget': () => (
+            <div className='common-row-wrap'>
+              <select>
+                <option value="Seurat">Seurat</option>
+              </select>
+        </div>
+          ),
         },
-        'ui:title': 'Max Genes', 
-      },
-      "show_umap": {
-        "classNames": "sub-category",
-        "ui:widget": "toggle"
-      },
-      "show_error": {
-        "classNames": "sub-category",
-        "ui:widget": "toggle"
-      },
+        "default_assay": {
+          "classNames": "sub-category",
+          'ui:widget': () => (
+            <div className='common-row-wrap'>
+              <span data-v-22825496="" class="ui-form-title-message warning"> * Optional </span>
+              <input type='text' />
+        </div>
+          ),
+        },
+        "layer": {
+          "classNames": "sub-category"
+        },
+        "path_of_scrublet_calls": {
+          "classNames": "sub-category"
+        },
+        "species": {
+          "classNames": "sub-category",
+          "ui:placeholder": "Select species type"
+        },
+        "idtype": {
+          "classNames": "sub-category"
+        },
+        "genes": {
+          "classNames": "sub-category",
+        },
+        "ncores": {
+          "classNames": "sub-category",
+          "ui:widget": "range",
+        },
+        "show_umap": {
+          "classNames": "sub-category",
+          "ui:widget": "toggle"
+        },
+        "show_error": {
+          "classNames": "sub-category",
+          "ui:widget": "toggle"
+        }
     }
-  }
-};
+  };
+  
