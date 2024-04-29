51894ac86bfc1cbbdae3cdb58c3b0921545b1503
cirisjl <cirisjl@gmail.com>
2024-04-29 10:25:35 -0400

    Added h5Seurat to file upload.

--- ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/uploadDataTask.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/publishDatasets/components/uploadDataTask.js
@@ -201,7 +201,7 @@ function getStandardFileName(fileName, fileType) {
         }));
     }
     else if(selectedFiles.length === 1) {
-        const acceptedFormats = [".tsv", ".csv", ".txt.gz", ".txt", ".h5ad", "rds", "h5seurat", "tsv.gz", "mtx.gz", "h5", "xlsx", "hdf5", "gz", "Robj", "zip", "rar", "tar", "tar.bz2", "tar.xz"];
+      const acceptedFormats = [".tsv", ".csv", ".txt.gz", ".txt", ".h5ad", "rds", "h5seurat", "h5Seurat", "tsv.gz", "mtx.gz", "h5", "xlsx", "hdf5", "gz", "Robj", "zip", "rar", "tar", "tar.bz2", "tar.xz"];
         if (!acceptedFormats.some(format => selectedFiles[0].endsWith(format))) {
             setErrorMessage("The selected file is not of an accepted standard format.");
             return;
