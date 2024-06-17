6abc7913c1d14e8da69e8e0af1e377215cb6599b
cirisjl <cirisjl@gmail.com>
2024-06-17 17:49:11 -0400

    Added React MarkDown.

--- oscb-ui/oscb-react/src/pages/tutorial.js
+++ oscb-ui/oscb-react/src/pages/tutorial.js
@@ -1,14 +1,49 @@
-// import LeftNav from "../components/leftNav";
+import { useState, useEffect } from "react"
+import axios from 'axios';
+import ReactMarkdown from "react-markdown"
+import LeftNav from "../components/LeftNavigation/leftNav";
+import gfm from "remark-gfm";
+import remarkImgToJsx from "remark-unwrap-images";
 import RightRail from "../components/RightNavigation/rightRail";
 
-export default function Docs() {
+const DIRECTUS_URL = `http://${process.env.REACT_APP_HOST_URL}:8055`
+
+
+export default function Tutorial() {
+    const [markdownText,setMarkdownText] = useState('')
+
+    useEffect(() => {
+        async function fetchFileData() {
+        try {
+            const response = await axios.get(DIRECTUS_URL + "/items/filemappings?filter[filename]=tutorial");
+            const data = response.data.data;
+            
+            if(data.length === 1) {
+                const fileMappingObject = data[0];
+                const fileID = fileMappingObject.fileID;
+                if(fileID !== null) {
+                    fetch(DIRECTUS_URL + "/assets/" + fileID)
+                    .then(response => response.text())
+                    .then(data => setMarkdownText(data))
+                    .catch(error => console.error('Error retrieving markdown:', error));
+                }
+            }
+            
+          } catch (error) {
+            console.error('Error retrieving data:', error);
+          }
+        }
+        
+        fetchFileData();
+      }, []);
+    
     return(
-        <div className="page-container">
+        <div className="tutorial-container">
             <div className="left-nav">
                 {/* <LeftNav /> */}
             </div>
             <div className="main-content">
-                <h1>I'm inside Docs page</h1>
+                <ReactMarkdown plugins={[gfm, remarkImgToJsx]} children={markdownText}/>
             </div>
             <div className="right-rail">
                 <RightRail />
