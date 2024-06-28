77f2fdc059ced5d6525a73951e97cc8450708720
U15EC104 <U15EC104@localhost>
2024-06-28 16:39:37 -0400

    Fixing the react-json-schema issue

--- ai-single-cell-ui/ai-single-cell-react/src/components/MyData/Workflows/workflowsComponent.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/MyData/Workflows/workflowsComponent.js
@@ -1,18 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { getCookie} from '../../../utils/utilFunctions';
-import { useNavigate } from 'react-router-dom';
-import schema from '../../../schema/react-json-schema/Workflows/clusteringUsingRaceIDSchema.json';
-import Form from 'react-jsonschema-form';
-import styled from 'styled-components';
-import { Container, Button } from "reactstrap";
-import Switch from 'react-switch';
-import Toggle from 'react-toggle';
-import 'react-toggle/style.css';
-import InputDataComponent from '../Tools/inputDataCollection';
+import React from 'react';
 import { ClusteringWorkFlowComponent } from './components/clusteringWorkflow';
-
-
-
 
 export function WorkflowsComponent(props) {
 
