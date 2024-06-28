17d20a5d365c796cf46b28db89a28aedb07c27c9
karthikgou <rajuraj244.kg@gmail.com>
2024-06-28 16:40:57 -0400

    Fixing the react-json-schema issue

--- ai-single-cell-ui/ai-single-cell-react/src/components/MyData/Workflows/components/clusteringWorkflow.js
+++ ai-single-cell-ui/ai-single-cell-react/src/components/MyData/Workflows/components/clusteringWorkflow.js
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import InputDataComponent from '../../Tools/inputDataCollection';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
 import Schema from '../../../../schema/react-json-schema/Workflows/clusteringWorkflow.json'
 import { uiSchema } from '../../../../schema/UI-schema/Workflows/clusteringWorkflow';
-// import Form from 'react-jsonschema-form';
 import Form from '@rjsf/core';
-import {validator} from '@rjsf/validator-ajv8';
+import validator from '@rjsf/validator-ajv8';
 import SelectComponent from '../../Tools/components/selectComponent';
 import GeneRangeSlider from '../../Tools/components/geneRangeSlider';
 import MultiSelectComponent from '../../Tools/components/multiselectComponent';
@@ -19,14 +13,6 @@ import UseDefaultSwitch from '../../Tools/components/useDefaultSwitch';
 import ClusterLabelInput from '../../Tools/components/customInputComponent';
 import Toggle from 'react-toggle';
 import 'react-toggle/style.css';
-// import Ajv from 'ajv';
-// import addKeywords from 'ajv-keywords';
-
-
-// // Create a custom Ajv instance
-// const ajv = new Ajv({ allErrors: true, $data: true });
-// addKeywords(ajv);
-
 
 export function ClusteringWorkFlowComponent(props) {
     const [selectedDatasets, setSelectedDatasets] = useState({});
@@ -83,17 +69,6 @@ export function ClusteringWorkFlowComponent(props) {
     const handleSubmit = ({ formData }) => {
         console.log("Submitted data:", formData);
     };
-
-    const CustomField = (props) => {
-      const { params_type, children } = props.formContext;
-      const fieldName = props.idSchema.$id.split('_').pop();
-    
-      if ((params_type === 'Normalization') ||
-          (params_type === 'Imputation')) {
-        return <div>{children}</div>;
-      }
-      return null;
-    };
     
   return (
     <div className='workflow-container common-class-tools-and-workflows'>
@@ -110,36 +85,15 @@ export function ClusteringWorkFlowComponent(props) {
             onSelectDataset={onSelectDataset} onDeleteDataset={onDeleteDataset}/>
       </div>
 
-      {/* <div>
-         <FormControl>
-            <FormLabel id="demo-row-radio-buttons-group-label">Choose</FormLabel>
-            <RadioGroup
-                row
-                aria-labelledby="demo-row-radio-buttons-group-label"
-                name="row-radio-buttons-group"
-                value={value}
-                onChange={handleChange}
-            >
-                <FormControlLabel value="normalization" control={<Radio />} label="Normalization" />
-                <FormControlLabel value="imputation" control={<Radio />} label="Imputation" />
-            </RadioGroup>
-            </FormControl>
-      </div> */}
-
       <div>
         <Form
-            // schema={getCombinedSchema()}
             schema = {Schema}
-            // uiSchema={uiSchema}
+            uiSchema={uiSchema}
             widgets={widgets}
             formData={formData}
             onChange={({ formData }) => setFormData(formData)}
             onSubmit={handleSubmit}
             onError={(errors) => console.log("Form errors:", errors)}
-            // fields={{ custom: CustomField }}
-            // formContext={{ params_type: formData.params_type }}
-            // ajv={ajv}
-
             validator={validator}
         />
       </div>
